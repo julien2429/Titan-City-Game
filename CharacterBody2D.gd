48d69f66a14c2d86631f99e512f4c5bae9df39c4
julien2429 <julieniancu@gmail.com>
2023-10-07 16:11:03 +0300

    Initial commit

---
+++ CharacterBody2D.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+@export var movespeed : float = 100
+@export var starting_direction : Vector2 = Vector2(0,1)
+# parameters/idle/blend_position
+	
+@onready var compass = $Compass
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
+
+func _ready():
+	update_animation_parameters(starting_direction)
+# Called when the node enters the scene tree for tshe first time.
+func _physics_process(delta):
+	
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	update_animation_parameters(input_direction)
+	velocity= movespeed * input_direction
+	move_and_slide()
+	pick_new_state()
+	rotate_compass()
+
+func update_animation_parameters( move_input : Vector2 ):
+	
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/idle/blend_position",move_input)
+		animation_tree.set("parameters/walk/blend_position",move_input)
+
+func rotate_compass():
+	var angle : float
+	var x: float = 0
+	var y: float = 0
+	
+	angle = atan2( x - position[0]  , -(y - position[1]) )
+	print(angle)
+	compass.rotation = angle + 3.14
+	
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("walk")
+	else:
+		state_machine.travel("idle")
